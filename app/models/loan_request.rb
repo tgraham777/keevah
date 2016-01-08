178d940e3e1ce485b536cf4569f9734fc29ff9b9
Tyler Graham <tylergraham134@gmail.com>
2016-01-07 17:51:46 -0700

    makes all the performance happen

--- app/models/loan_request.rb
+++ app/models/loan_request.rb
@@ -81,6 +81,8 @@ class LoanRequest < ActiveRecord::Base
   end
 
   def related_projects
-    (categories.flat_map(&:loan_requests) - [self]).shuffle.take(4)
+    Rails.cache.fetch("related-projects") do
+      (categories.flat_map(&:loan_requests) - [self]).shuffle.take(4)
+    end
   end
 end
